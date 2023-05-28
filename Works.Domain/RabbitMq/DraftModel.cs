afc54aa7d4ed03a379c509b86fe8a08d7fd4a74a
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-05-28 20:43:59 +0300

    feat: added models for RabbitMQ subscriber FQW-Works-9

---
+++ Works.Domain/RabbitMq/DraftModel.cs
@@ -0,0 +1,12 @@
+namespace FavoriteLiterature.Works.Domain.RabbitMq;
+
+public class DraftModel
+{
+    public string Name { get; set; } = null!;
+    
+    public string? Description { get; set; }
+    
+    public List<Guid> Authors { get; set; }
+    
+    public List<Guid> Genres { get; set; }
+}
